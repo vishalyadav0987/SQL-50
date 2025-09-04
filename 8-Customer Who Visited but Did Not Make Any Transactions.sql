b8001a90f291fa9575b462472ade121824e78ab7
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-09-04 19:23:00 +0530

    SQL 8

---
+++ 8-Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,6 @@
+SELECT V.customer_id , COUNT(V.customer_id) AS count_no_trans
+FROM Visits AS V
+LEFT JOIN Transactions AS T
+ON V.visit_id = T.visit_id
+WHERE T.transaction_id IS NULL
+GROUP BY V.customer_id
